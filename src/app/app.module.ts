9cf47afd5a50d7f2b626d68e78406193565e8f3b
Damian Arp <damianarp@gmail.com>
2021-01-06 11:18:25 -0300

    Creación del componente Persona (con CLI de Angular)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PersonasComponent
+    AppComponent, PersonasComponent, PersonaComponent
   ],
   imports: [
     BrowserModule
