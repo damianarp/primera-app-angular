b78cdb18b59352dc180451ca162184eb58c8766f
Damian Arp <damianarp@gmail.com>
2021-01-06 11:10:41 -0300

    Creación manual del componente Personas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PersonasComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/personas/personas.component.html
@@ -0,0 +1,1 @@
+<h1>Listado de Personas</h1>
