a98ec6333aaeba87f213f2c78901cfa0fa16b64d
Damian Arp <damianarp@gmail.com>
2021-01-06 11:31:49 -0300

    Aplicando algunos estilos CSS

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  templateUrl: './personas.component.html'
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent {
 
